133b8acc729d09d6337df4d7b041e0ede090028b
unknown <gayeongit@gmail.com>
2023-12-16 04:08:40 +0900

    v8.4

--- random.py
+++ random.py
@@ -2,17 +2,14 @@ import random
 from picamera import PiCamera
 
 def camerasys():
-    def randnum():
-        return random.choice([0, 1])
-    
     camera=PiCamera()
 
     while(1):
-        pir=randnum()   # pir 센서
-        bt=randnum()    # 버튼
+        pir=random.choice([0, 1])   # pir 센서
+        bt=random.choice([0, 1])    # 버튼
 
         if(pir==1):  # pir 센서가 사람을 감지하면
-            camera.start_recording('./video{}') # 영상 촬영 시작
+            camera.start_recording('./video{}.h264') # 영상 촬영 시작
 
             if(bt==1): # 버튼을 누르면
                 camera.capture('./photo{}.jpg')   # 사진이 촬영됨
