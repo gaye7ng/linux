5cac1202fe8472a9f274236d4f45d21a8490938c
unknown <gayeongit@gmail.com>
2023-12-16 06:05:44 +0900

    v10.1

--- camerasys.py
+++ camerasys.py
@@ -9,16 +9,16 @@ def camerasys():
         bt=random.choice([0, 1])    # 버튼
 
         if(pir==1):  # pir 센서가 사람을 감지하면
-            camera.start_recording('./video.h264') # 영상 촬영 시작
+            camera.start_recording('./cctv.h264') # 영상 촬영 시작
 
             if(bt==1): # 버튼을 누르면
-                camera.capture('./photo.jpg')   # 사진이 촬영됨
+                camera.capture('./cctv_photo.jpg')   # 사진이 촬영됨
             
             camera.wait_recording(5)
             camera.stop_recording()
         else:
             if(bt==1):   # 버튼을 누르면
-                camera.capture('./photo.jpg')   # 사진이 촬영됨
+                camera.capture('./cctv_photo.jpg')   # 사진이 촬영됨
     
 try:
     camerasys()
